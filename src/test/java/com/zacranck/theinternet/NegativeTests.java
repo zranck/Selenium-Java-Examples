736e8fb886df0c4064b89cc4d2b1920f59fa852e
zranck <Meeko21@>
2019-04-08 09:44:47 -0600

    Adds more crossbrowser testing and uses the Parameter annotation

--- src/test/java/com/zacranck/theinternet/NegativeTests.java
+++ src/test/java/com/zacranck/theinternet/NegativeTests.java
@@ -4,25 +4,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class NegativeTests {
+    WebDriver driver;
 
-    @Parameters({ "username", "password", "expectedErrorMessage" })
-    @Test()
-    public void negativeTest(String username, String password, String expectedErrorMessage){
-        System.out.println("Starting Negative Test");
-
-
+    @Parameters({"browser"})
+    @BeforeMethod
+    protected void setUp(@Optional("chrome") String browser) {
         // Create Driver
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        System.out.println("Create driver: " + browser);
+
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Do not know hot to start: " + browser + " starting chrome instead.");
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+        }
+
 
         sleep(3000); // for demo purposes only
 
         driver.manage().window().maximize();
+    }
+
+    @Parameters({"username", "password", "expectedErrorMessage"})
+    @Test()
+    public void negativeTest(String username, String password, String expectedErrorMessage) {
+        System.out.println("Starting Negative Test");
+
 
         // Open Page
         String url = "https://the-internet.herokuapp.com/login";
@@ -43,17 +66,24 @@ public class NegativeTests {
         WebElement errorMessage = driver.findElement(By.id("flash"));
 
         String actualErrorMessage = errorMessage.getText();
-        Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage),"Error message is not displayed\nActual Message: " + actualErrorMessage);
+        Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage), "Error message is not displayed\nActual Message: " + actualErrorMessage);
 
+
+    }
+
+    @AfterMethod
+    protected void tearDown() {
         sleep(3000);
-
+        System.out.println("Close driver");
+        // Close driver
         driver.quit();
     }
+
     private void sleep(long millis) {
         // Purposely adding this implicit wait for demo purposes
-        try{
+        try {
             Thread.sleep(millis);
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
